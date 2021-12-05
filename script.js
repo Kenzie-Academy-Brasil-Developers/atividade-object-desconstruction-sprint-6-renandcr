bd0036eca25542886d96bb26429fa192efac5354
renandcr <renandamagazine@gmail.com>
2021-12-05 18:58:33 -0300

    Entregar simples tarefa de treinamento.

--- script.js
+++ script.js
@@ -28,3 +28,68 @@ const itemArray = [
         "section": "Hortifruti"
     },
 ]
+
+const testMap = (array) => {
+    const result = array.map((value) => {
+        let {id, price} = value
+        if(id > 2){
+            price = 2.00
+        } 
+        return {...value, price}
+    })
+    return result
+}
+// console.log(testMap(itemArray))
+
+
+const testFilter = (array) => {
+    const result = array.filter(({id}) => {
+        if(id <= 2){
+            return {...array}
+        }
+    })
+    return result
+}
+// console.log(testFilter(itemArray))
+            
+const testFind = (array) => {
+    const result = array.find(({id, product}) => {
+        if(product === "Leite"){
+            return {...array}
+        }
+    })
+    return result
+}
+// console.log(testFind(itemArray))
+
+const testReduce = (array) => {
+    const totalList = array.reduce((acc,{quantity, price}) => {
+        acc += quantity * price
+        return acc
+    },0)
+    return totalList
+}
+// console.log(testReduce(itemArray))
+
+const higherPrice = (array) => {
+    const result = array.filter(({price}) => {
+        if(price > 3){
+            return {...array}
+        }
+    })
+    return result
+}
+// console.log(higherPrice(itemArray))
+
+const changePrice = (array) => {
+    const result = array.map((value) => {
+        let {product, price} = value
+        if(product === "Leite"){
+            price = 4
+        }
+        return {...value, price}
+    })
+    return result
+}
+// console.log(changePrice(itemArray))
+
